78d5a120ea39c4cbfbecbf00b02e7c631ceb7fc3
Jeel Patel <pateljeel3105@gmail.com>
2025-08-18 06:59:57 -0400

    Time: 0 ms (100.00%) | Memory: 33.6 MB (52.86%) - LeetSync

--- Solutions/617-merge-two-binary-trees/merge-two-binary-trees.cpp
+++ Solutions/617-merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -11,50 +11,50 @@
  */
 
 
-void inorder(TreeNode* root, vector<int> &in){
-    if(root == NULL){
-        return;
-    }
-    inorder(root->left, in);
-    in.push_back(root->val);
-    inorder(root->right, in);
-}
+// void inorder(TreeNode* root, vector<int> &in){
+//     if(root == NULL){
+//         return;
+//     }
+//     inorder(root->left, in);
+//     in.push_back(root->val);
+//     inorder(root->right, in);
+// }
 
-vector<int> mergeArrays(vector<int> a, vector<int> b){
-    vector<int> ans(a.size() + b.size());
-    int i = 0, j = 0, k = 0;
-    while( i < a.size() && j<b.size()){
-        if(a[i] < b[j]){
-            ans[k++] = a[i];
-            i++;
-        }
-        else{
-            ans[k++] = b[j];
-            j++;
-        }
-    }
-    while(i<a.size()){
-        ans[k++] = a[i];
-        i++;
-    }
-    while(j<b.size()){
-        ans[k++] = b[j];
-        j++;
-    }
-    return ans;
-}
+// vector<int> mergeArrays(vector<int> a, vector<int> b){
+//     vector<int> ans(a.size() + b.size());
+//     int i = 0, j = 0, k = 0;
+//     while( i < a.size() && j<b.size()){
+//         if(a[i] < b[j]){
+//             ans[k++] = a[i];
+//             i++;
+//         }
+//         else{
+//             ans[k++] = b[j];
+//             j++;
+//         }
+//     }
+//     while(i<a.size()){
+//         ans[k++] = a[i];
+//         i++;
+//     }
+//     while(j<b.size()){
+//         ans[k++] = b[j];
+//         j++;
+//     }
+//     return ans;
+// }
 
 
-TreeNode *inordertobest(int s, int e, vector<int> &in){
-    if(s>e){
-        return NULL;
-    }
-    int mid = s + (e-s)/2;
-    TreeNode* root = new TreeNode (in[mid]);
-    root->left = inordertobest(s, mid-1, in);
-    root->right = inordertobest(mid+1, e, in);
-    return root;
-}
+// TreeNode *inordertobest(int s, int e, vector<int> &in){
+//     if(s>e){
+//         return NULL;
+//     }
+//     int mid = s + (e-s)/2;
+//     TreeNode* root = new TreeNode (in[mid]);
+//     root->left = inordertobest(s, mid-1, in);
+//     root->right = inordertobest(mid+1, e, in);
+//     return root;
+// }
 
 
 class Solution {
