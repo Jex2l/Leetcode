b72dbf357d338d5ff50fc381b3efa0be18208a9e
Jeel Patel <pateljeel3105@gmail.com>
2025-08-29 20:36:47 -0400

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.38%) - LeetSync

--- Solutions/3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ Solutions/3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long x =n;
-        long long y= m;
-        return x*y/2;
+        return long(n)*long(m)/long(2);
     }
 };
