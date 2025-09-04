bb73d8fdf499f2c574add259306d0d26b422f151
Jeel Patel <pateljeel3105@gmail.com>
2025-09-04 06:09:15 -0400

    Time: 20 ms (5.13%) | Memory: 19.6 MB (5.40%) - LeetSync

--- Solutions/14-longest-common-prefix/longest-common-prefix.cpp
+++ Solutions/14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,20 +1,78 @@
+class TrieNode{
+    public:
+        char data;
+        TrieNode* children[26];
+        int childCount;
+        bool isTerminal;
+
+        TrieNode(char ch){
+            data = ch;
+            for(int i=0; i<26; i++){
+                children[i] = NULL;
+            }
+            childCount = 0;
+            isTerminal = false;
+        }
+};
+
+class Trie{
+    public:
+        TrieNode* root;
+    
+    Trie(char ch){
+        root = new TrieNode(ch);
+    }
+    void insertUtil(TrieNode* root, string word){
+        if(word.length() == 0){
+            root->isTerminal = true;
+            return;
+        }
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if(root->children[index] != NULL){
+            child = root->children[index];
+        }
+        else{
+            child = new TrieNode(word[0]);
+            root->childCount++;
+            root->children[index] = child;
+        }
+        insertUtil(child, word.substr(1));
+    }
+    
+    void insert(string word) {
+        insertUtil(root, word);
+    }
+    void lcp(string str, string&ans){
+        for(int i = 0; str.length(); i++){
+            char ch = str[i];
+            if(root->childCount == 1){
+                ans.push_back(ch);
+                int index = ch - 'a';
+                root = root->children[index];
+            }
+            else{
+                break;
+            }
+            if(root->isTerminal){
+                break;
+            }
+        }
+    }
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return "";
-        string ans = "";
-        for (size_t i = 0; i < strs[0].size(); ++i) {
-            char ch = strs[0][i];
-            bool match = true;
-            for (size_t j = 1; j < strs.size(); ++j){
-                if (i >= strs[j].size() || strs[j][i] != ch){
-                    match = false;
-                    break;
-                }
-            }
-            if (!match) break;
-            ans.push_back(ch);
+        Trie *t = new Trie('\0');
+        for(int i =0; i<strs.size(); i++){
+            if (strs[i].empty()) return "";
+            t->insert(strs[i]);
         }
+        string first = strs[0];
+        string ans = "";
+        t->lcp(first, ans);
         return ans;
     }
 };
