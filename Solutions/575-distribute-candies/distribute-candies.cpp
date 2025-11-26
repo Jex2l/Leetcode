9f0d8d8f6c5e17258a1e50af23d249be62708873
Jeel Patel <pateljeel3105@gmail.com>
2025-11-26 05:14:57 -0500

    Time: 969 ms (5.23%) | Memory: 92 MB (89.76%) - LeetSync

---
+++ Solutions/575-distribute-candies/distribute-candies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        sort(candyType.begin(), candyType.end());
+        int n = candyType.size();
+        vector<int> x;
+        x.push_back(candyType[0]);
+        int a = 1;
+        for(int i = 1; i<n; i++){
+            if(x[a-1] != candyType[i]){
+                x.push_back(candyType[i]);
+                a++;
+                cout<<x[a-1];
+                cout<<a;
+            }
+        }
+        if(n/2 >= a){
+            return a;
+        }
+        return n/2;
+    }
+};
