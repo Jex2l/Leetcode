071303d7525c42012af7b65e1fcb4715ca935714
Jeel Patel <pateljeel3105@gmail.com>
2025-11-27 03:02:43 -0500

    Time: 3 ms (40.38%) | Memory: 11.2 MB (34.11%) - LeetSync

--- Solutions/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ Solutions/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -3,6 +3,8 @@ public:
     string reverseWords(string s) {
         vector<string> words;
         string temp = "";
+
+        // Collect words, ignoring multiple spaces
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == ' ') {
                 if (!temp.empty()) {
@@ -13,13 +15,20 @@ public:
                 temp.push_back(s[i]);
             }
         }
+
+        // Push last word if exists
         if (!temp.empty()) words.push_back(temp);
+
+        // Reverse words 
         reverse(words.begin(), words.end());
+
+        // Build answer with single-space separation
         string ans = "";
         for (int i = 0; i < words.size(); i++) {
             ans += words[i];
             if (i != words.size() - 1) ans += " ";
         }
+
         return ans;
     }
 };
