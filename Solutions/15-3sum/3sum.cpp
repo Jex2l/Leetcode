34a64529848e6242ab16e86eb6adf66dfc8a6761
Jeel Patel <pateljeel3105@gmail.com>
2025-11-29 07:15:56 -0500

    Time: 2929 ms (5.00%) | Memory: 447.2 MB (5.06%) - LeetSync

--- Solutions/15-3sum/3sum.cpp
+++ Solutions/15-3sum/3sum.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i + 1;
-            int k = n - 1;
-            while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0) {
-                    j++;
-                }
-                else if(sum > 0) {
-                    k--;
-                }
-                else {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
-                    while(j < k && nums[j] == nums[j - 1]) j++;
-                    while(j < k && nums[k] == nums[k + 1]) k--;
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            int one = nums[i];
+            int target = -one;
+            int s = i + 1;
+            int e = n - 1;
+            while (s < e) {
+                long long sum = (long long)nums[s] + nums[e];
+                if (sum == target) {
+                    res.push_back({one, nums[s], nums[e]});
+                    s++;
+                    e--;
+                } else if (sum < target) {
+                    s++;
+                } else {
+                    e--;
                 }
             }
         }
-        return ans;
+        sort(res.begin(), res.end());
+        res.erase(unique(res.begin(), res.end()), res.end());
+        return res;
     }
 };
