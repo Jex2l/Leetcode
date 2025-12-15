c74be8fb5cbe3772a2da49aed503f895a1fee657
Jeel Patel <pateljeel3105@gmail.com>
2025-12-15 05:06:00 -0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (38.51%) - LeetSync

--- Solutions/88-merge-sorted-array/merge-sorted-array.cpp
+++ Solutions/88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int j = 0, i = m; j<n; j++){
-            nums1[i] = nums2[j];
-            i++;
+        // BrutForce
+        // for (int j = 0, i = m; j<n; j++){
+        //     nums1[i] = nums2[j];
+        //     i++;
+        // }
+        // sort(nums1.begin(),nums1.end());
+
+        vector<int> result;
+        int l1 = 0;
+        int l2 = 0;
+        for(int i = 0; i< m + n; i++){
+            if(l2 < nums2.size()){
+                if((nums1[l1] < nums2[l2]) && (l1 < m)){
+                    result.push_back(nums1[l1]);
+                    l1++;
+                }
+                else{
+                    result.push_back(nums2[l2]);
+                    l2++;
+                }
+            }
+            else{
+                result.push_back(nums1[l1]);
+                l1++;
+            }
         }
-        sort(nums1.begin(),nums1.end());
+        nums1 = result;
     }
 };
