8cfe641ad66f1950100c22a132fdc9ba88b41b68
Jeel Patel <pateljeel3105@gmail.com>
2025-12-15 08:45:14 -0500

    Time: 2033 ms (1.98%) | Memory: 8.5 MB (91.81%) - LeetSync

--- Solutions/50-powx-n/powx-n.cpp
+++ Solutions/50-powx-n/powx-n.cpp
@@ -1,62 +1,43 @@
-// class Solution {
-// public:
-//     double myPow(double x, int n) {
-//         if(x == 1) return x;
-//         if(n == INT_MAX){
-//             if((n %2 == 0) && x == -1){
-//                 return 1;
-//             }
-//             if((n %2 != 0) && x == -1){
-//                 return -1;
-//             }  
-//         }
-//         if(n == INT_MIN){
-//             if((n %2 == 0) && x == -1){
-//                 return 1;
-//             }
-//             if((n %2 != 0) && x == -1){
-//                 return -1;
-//             }
-//             else return 0;  
-//         }
-//         double result = 1;
-//         if(n > 0){
-//             while( n > 0){
-//                 result = result * x;
-//                 n--;
-//             }
-//         }
-//         if(n < 0){
-//             while(n < 0){
-//                 result = result/x;
-//                 n++;
-//             }
-//         }
-//         return result;
-//     }
-// };
-
 class Solution {
 public:
     double myPow(double x, int n) {
-        
+        if (x == 1.0) return 1.0;
+        if (x == -1.0) return (n & 1) ? -1.0 : 1.0;
+        if (n == INT_MIN) return 0.0;
         if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        long num = labs(n);
-        
-        double pow = 1;
-        
-        while(num){
-            if(num & 1) {
-                pow *= x;
-            }
-            
-            x *= x;
-            num >>= 1;
+            x = 1/x;
+            n=-n;
         }
-        
-        return pow;
+        double result = 1;
+        while( n > 0){
+            result = result * x;
+            n--;
+        }
+        return result;
     }
 };
+
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+        
+//         if(n < 0) {
+//             x = 1 / x;
+//         } 
+        
+//         long num = labs(n);
+        
+//         double pow = 1;
+        
+//         while(num){
+//             if(num & 1) {
+//                 pow *= x;
+//             }
+            
+//             x *= x;
+//             num >>= 1;
+//         }
+        
+//         return pow;
+//     }
+// };
