14a6ffffbcb5fa1fec8b44878bfed1d223fcdc96
Jeel Patel <pateljeel3105@gmail.com>
2026-01-31 00:29:36 -0500

    Time: 0 ms (100.00%) | Memory: 14.8 MB (36.43%) - LeetSync

--- Solutions/1-two-sum/two-sum.cpp
+++ Solutions/1-two-sum/two-sum.cpp
@@ -11,41 +11,52 @@ public:
         //     }
         // }
         // return a;
-        vector<int> d = nums;
-        sort(d.begin(), d.end());
-        vector<int> x;
-        int s = 0;
-        int e = d.size() - 1;
-        while(s <= e){
-            int sum = d[s] + d[e];
-            if(sum == target){
-                x.push_back(d[s]);
-                x.push_back(d[e]);
-                break;
-            }
-            else if(sum < target){
-                s++;
-            }
-            else{
-                e--;
-            }
-        }
-        int first = -1, second = -1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == x[0]) {
-                first = i;
-                break;
-            }
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == x[1] && i != first) {
-                second = i;
-                break;
-            }
-        }
 
-        x[0] = first;
-        x[1] = second;
-        return x;
+
+        // vector<int> d = nums;
+        // sort(d.begin(), d.end());
+        // vector<int> x;
+        // int s = 0;
+        // int e = d.size() - 1;
+        // while(s <= e){
+        //     int sum = d[s] + d[e];
+        //     if(sum == target){
+        //         x.push_back(d[s]);
+        //         x.push_back(d[e]);
+        //         break;
+        //     }
+        //     else if(sum < target){
+        //         s++;
+        //     }
+        //     else{
+        //         e--;
+        //     }
+        // }
+        // int first = -1, second = -1;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] == x[0]) {
+        //         first = i;
+        //         break;
+        //     }
+        // }
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] == x[1] && i != first) {
+        //         second = i;
+        //         break;
+        //     }
+        // }
+
+        // x[0] = first;
+        // x[1] = second;
+        // return x;
+
+        unordered_map<int,int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+            if (mp.count(need))
+                return {mp[need], i};
+            mp[nums[i]] = i;
+        }
+        return {};
     }
 };
