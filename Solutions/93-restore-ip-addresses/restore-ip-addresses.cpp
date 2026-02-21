42109ee32eae24e070af6cad4fe59d8479af7fc8
Jeel Patel <pateljeel3105@gmail.com>
2026-02-21 16:12:05 -0500

    Time: 2 ms (44.24%) | Memory: 10.3 MB (75.97%) - LeetSync

--- Solutions/93-restore-ip-addresses/restore-ip-addresses.cpp
+++ Solutions/93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -1,36 +1,20 @@
 class Solution {
-    bool valid(const string& s, int start, int length) {
-        return length == 1 || (s[start] != '0' && (length < 3 || s.substr(start, length) <= "255"));
-    }
-    void helper(const string& s, int startIndex, vector<int>& dots, vector<string>& ans) {
-        const int remainingLength = s.length() - startIndex;
-        const int remainingNumberOfIntegers = 4 - dots.size();
-        if (remainingLength > remainingNumberOfIntegers * 3 || remainingLength < remainingNumberOfIntegers) return;
-        if (dots.size() == 3) {
-            if (valid(s, startIndex, remainingLength)) {
-                string temp;
-                int last = 0;
-                for (int dot : dots) {
-                    temp.append(s.substr(last, dot));
-                    last += dot;
-                    temp.append(".");
-                }
-                temp.append(s.substr(startIndex));
-                ans.push_back(temp);
-            }
-            else return;
-        }
-        for (int curPos = 1; curPos <= 3 && curPos <= remainingLength; ++curPos) {
-            dots.push_back(curPos);
-            if (valid(s, startIndex, curPos)) helper(s, startIndex + curPos, dots, ans);
-            dots.pop_back();
-        }
-    }
 public:
+    void backtrack(string& s, int length, vector<string>& result, vector<string>& currentSegments, int startindex) {
+        if (currentSegments.size() == 4) {
+            if (startindex == length)  result.push_back(currentSegments[0] + "." + currentSegments[1] + "." + currentSegments[2] + "." + currentSegments[3]);
+            return;}
+        int currentnum = 0;
+        for(int i = startindex; i < min(length, startindex + 3); i++) {
+            currentnum = currentnum * 10 + (s[i] - '0');
+            if(currentnum > 255) break;
+            if(i > startindex && s[startindex] == '0') break;
+            currentSegments.push_back(s.substr(startindex, i - startindex + 1));
+            backtrack(s, length, result, currentSegments, i + 1);
+            currentSegments.pop_back();}}
     vector<string> restoreIpAddresses(string s) {
-        vector<int> dots;
-        vector<string> ans;
-        helper(s, 0, dots, ans);
-        return ans;
-    }
-};
+        int length = s.size();
+        vector<string> result;
+        vector<string> currentSegments;
+        backtrack(s, length, result, currentSegments, 0);
+        return result;}};
