ee5911492aab2cda0205e3ba3f4d0d8750d2fc64
Jeel Patel <pateljeel3105@gmail.com>
2026-02-25 22:02:28 -0500

    Time: 0 ms (100.00%) | Memory: 13.9 MB (97.46%) - LeetSync

--- Solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ Solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -4,9 +4,8 @@ public:
     int pivot(vector<int> &arr, int lo, int hi) {
         while (lo < hi) {
             if (arr[lo] < arr[hi]) return lo;
-            int mid = lo + (hi - lo) / 2;
-            if (arr[mid] > arr[hi]) lo = mid + 1;
-            else hi = mid;
+            if (arr[(lo + (hi - lo) / 2)] > arr[hi]) lo = (lo + (hi - lo) / 2) + 1;
+            else hi = (lo + (hi - lo) / 2);
         }
         return lo;
     }
