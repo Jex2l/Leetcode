15b20ce9569420f07ae6ac675bf0ba1fe7def222
Jeel Patel <pateljeel3105@gmail.com>
2026-02-26 12:17:19 -0500

    Time: 53 ms (35.03%) | Memory: 29.2 MB (45.61%) - LeetSync

--- Solutions/15-3sum/3sum.cpp
+++ Solutions/15-3sum/3sum.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
         vector<vector<int>> ans;
-        for(int i = 0; i < n; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int target = -nums[i];
-            unordered_set<int> seen;
-            for(int j = i + 1; j < n; j++) {
-                int complement = target - nums[j];
-                if(seen.count(complement)) {
-                    ans.push_back({nums[i], complement, nums[j]});
-                    while(j + 1 < n && nums[j] == nums[j+1]) j++;
+        int n = nums.size();
+        for(int i = 0; i < n - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
                 }
-                seen.insert(nums[j]);
+                else if(sum < 0) left++;
+                else right--;
             }
         }
         return ans;
