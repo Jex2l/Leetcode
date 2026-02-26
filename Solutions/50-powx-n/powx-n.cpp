ea1e1777329fcb039467046ca93b89f8ee9352c1
Jeel Patel <pateljeel3105@gmail.com>
2026-02-26 15:47:46 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.68%) - LeetSync

--- Solutions/50-powx-n/powx-n.cpp
+++ Solutions/50-powx-n/powx-n.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (x == 1.0) return 1.0;
-        if (x == -1.0) return (n & 1) ? -1.0 : 1.0;
-        if (n == INT_MIN) return 0.0;
-        if(n < 0) {
-            x = 1/x;
-            n=-n;
-        }
+        long long N = n;
         double result = 1;
-        while( n > 0){
-            result = result * x;
-            n--;
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+        while(N > 0){
+            if(N % 2 == 1) result *= x;
+            x *= x;
+            N /= 2;
         }
         return result;
     }
 };
-
-// class Solution {
-// public:
-//     double myPow(double x, int n) {
-        
-//         if(n < 0) {
-//             x = 1 / x;
-//         } 
-        
-//         long num = labs(n);
-        
-//         double pow = 1;
-        
-//         while(num){
-//             if(num & 1) {
-//                 pow *= x;
-//             }
-            
-//             x *= x;
-//             num >>= 1;
-//         }
-        
-//         return pow;
-//     }
-// };
