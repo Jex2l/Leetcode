fea59ed9670bde0affaf3c161ecfc48631596e5f
Jeel Patel <pateljeel3105@gmail.com>
2026-03-04 11:53:25 -0500

    Time: 8 ms (41.05%) | Memory: 16 MB (5.28%) - LeetSync

--- Solutions/1-two-sum/two-sum.cpp
+++ Solutions/1-two-sum/two-sum.cpp
@@ -1,62 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // vector<int>a;
-        // for(int i = 0; i<nums.size(); i++){
-        //     for(int j = i+1; j<nums.size(); j++){
-        //         if(nums[i]+nums[j] == target){
-        //             a.push_back(i);
-        //             a.push_back(j);
-        //         }
-        //     }
-        // }
-        // return a;
-
-
-        // vector<int> d = nums;
-        // sort(d.begin(), d.end());
-        // vector<int> x;
-        // int s = 0;
-        // int e = d.size() - 1;
-        // while(s <= e){
-        //     int sum = d[s] + d[e];
-        //     if(sum == target){
-        //         x.push_back(d[s]);
-        //         x.push_back(d[e]);
-        //         break;
-        //     }
-        //     else if(sum < target){
-        //         s++;
-        //     }
-        //     else{
-        //         e--;
-        //     }
-        // }
-        // int first = -1, second = -1;
-        // for (int i = 0; i < nums.size(); i++) {
-        //     if (nums[i] == x[0]) {
-        //         first = i;
-        //         break;
-        //     }
-        // }
-        // for (int i = 0; i < nums.size(); i++) {
-        //     if (nums[i] == x[1] && i != first) {
-        //         second = i;
-        //         break;
-        //     }
-        // }
-
-        // x[0] = first;
-        // x[1] = second;
-        // return x;
-
-        unordered_map<int,int> mp;
-        for (int i = 0; i < nums.size(); i++) {
-            int need = target - nums[i];
-            if (mp.count(need))
-                return {mp[need], i};
-            mp[nums[i]] = i;
+        unordered_map<int, int> x;
+        vector<int> ans;
+        for(int i = 0; i<nums.size(); i++){
+            int complement = target - nums[i];
+            if(x.count(complement)){
+                ans.push_back(i);
+                ans.push_back(x[complement]);
+            }
+            x[nums[i]] = i;
         }
-        return {};
+        return ans;
     }
 };
