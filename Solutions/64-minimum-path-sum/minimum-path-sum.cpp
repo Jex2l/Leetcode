02ba9cd0bb891b1acb615654928198e7f620e9f8
Jeel Patel <pateljeel3105@gmail.com>
2026-03-06 12:08:50 -0500

    Time: 0 ms (100.00%) | Memory: 15.3 MB (87.98%) - LeetSync

--- Solutions/64-minimum-path-sum/minimum-path-sum.cpp
+++ Solutions/64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,19 +1,36 @@
+// class Solution {
+// public:
+//     int mins(vector<vector<int>>& grid, int row, int column, vector<vector<int>>& dp){
+//         int rs = grid.size();
+//         int cs = grid[0].size();
+//         if(row >= rs || column >= cs) return INT_MAX;
+//         if(row == rs - 1 && column == cs - 1) return grid[row][column];
+//         if(dp[row][column] != -1) return dp[row][column];
+//         int right = mins(grid, row, column + 1, dp);
+//         int down  = mins(grid, row + 1, column, dp);
+//         return dp[row][column] = grid[row][column] + min(right, down);
+//     }
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int rs = grid.size();
+//         int cs = grid[0].size();
+//         vector<vector<int>> dp(rs, vector<int>(cs, -1));
+//         return mins(grid, 0, 0, dp);
+//     }
+// };
+
 class Solution {
 public:
-    int mins(vector<vector<int>>& grid, int row, int column, vector<vector<int>>& dp){
-        int rs = grid.size();
-        int cs = grid[0].size();
-        if(row >= rs || column >= cs) return INT_MAX;
-        if(row == rs - 1 && column == cs - 1) return grid[row][column];
-        if(dp[row][column] != -1) return dp[row][column];
-        int right = mins(grid, row, column + 1, dp);
-        int down  = mins(grid, row + 1, column, dp);
-        return dp[row][column] = grid[row][column] + min(right, down);
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        int rs = grid.size();
-        int cs = grid[0].size();
-        vector<vector<int>> dp(rs, vector<int>(cs, -1));
-        return mins(grid, 0, 0, dp);
+        int r = grid.size();
+        int c = grid[0].size();
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(i == 0 && j == 0) continue;
+                if(i == 0) grid[i][j] += grid[i][j-1];
+                else if(j == 0) grid[i][j] += grid[i-1][j];
+                else grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[r-1][c-1];
     }
 };
