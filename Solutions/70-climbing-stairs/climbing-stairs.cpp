73a0d60f9e1f2c2bd3f04415b531bb24e29c9be1
Jeel Patel <pateljeel3105@gmail.com>
2026-03-08 16:01:24 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.24%) - LeetSync

--- Solutions/70-climbing-stairs/climbing-stairs.cpp
+++ Solutions/70-climbing-stairs/climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int climbStairs(int n) {
         if(n <= 2) return n;
         int a = 1, b = 2;
-        for(int i = 3; i <= n; i++){
+        for(int i = 3; i<=n; i++){
             int c = a + b;
             a = b;
             b = c;
