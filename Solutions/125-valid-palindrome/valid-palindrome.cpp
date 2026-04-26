eec8f9108c7a051e0a42f6afade9f8ec6e19df6f
Jeel Patel <pateljeel3105@gmail.com>
2026-04-25 23:41:38 -0400

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.01%) - LeetSync

--- Solutions/125-valid-palindrome/valid-palindrome.cpp
+++ Solutions/125-valid-palindrome/valid-palindrome.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (isalnum(s[i])) {
-                temp.push_back(tolower(s[i]));
-            }
+        string x;
+        for(char c : s) {
+            if(isalnum(c)) x.push_back(tolower(c));
         }
-        int st = 0;
-        int e = temp.length() - 1;
-        while (st < e) {
-            if (temp[st] != temp[e]) {
-                return false;
-            }
-            st++;
-            e--;
-        }
-        return true;
+        string y = x;
+        reverse(y.begin(), y.end());
+        return x == y;
     }
 };
