1bc2987443e66fe5344f86e69f34548d2548d9ed
Jeel Patel <pateljeel3105@gmail.com>
2026-04-27 15:59:35 -0400

    Time: 4 ms (68.96%) | Memory: 13.7 MB (70.23%) - LeetSync

--- Solutions/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ Solutions/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,17 +1,34 @@
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         string x;
+//         stack<char> y;
+//         for(int i = 0; i<s.size(); i++){
+//             if(y.empty() || y.top() != s[i]) y.push(s[i]);
+//             else y.pop();
+//         }
+//         while(!y.empty()){
+//             x.push_back(y.top());
+//             y.pop();
+//         }
+//         reverse(x.begin(), x.end());
+//         return x;
+//     }
+// };
+
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string x;
-        stack<char> y;
-        for(int i = 0; i<s.size(); i++){
-            if(y.empty() || y.top() != s[i]) y.push(s[i]);
-            else y.pop();
+        string st;
+
+        for(char c : s) {
+            if(!st.empty() && st.back() == c) {
+                st.pop_back();
+            } else {
+                st.push_back(c);
+            }
         }
-        while(!y.empty()){
-            x.push_back(y.top());
-            y.pop();
-        }
-        reverse(x.begin(), x.end());
-        return x;
+
+        return st;
     }
 };
