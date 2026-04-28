5ba993bc9be4a7479ca76d2239c8bb2b9a169981
Jeel Patel <pateljeel3105@gmail.com>
2026-04-28 19:38:58 -0400

    Time: 0 ms (100.00%) | Memory: 23.6 MB (72.07%) - LeetSync

--- Solutions/543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ Solutions/543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -9,38 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// int maxDepth(TreeNode* root) {
-//     if(root == NULL){
-//         return 0;
-//     }
-//     int left = maxDepth(root -> left);
-//     int right = maxDepth(root -> right);
-//     int ans = max(left, right) + 1;
-//     return ans;
-// }
-
-pair<int, int> diameterFast(TreeNode* root){
-    if(root == NULL){
-        pair<int, int> p = make_pair(0,0);
-        return p;
-    }
-    pair<int, int> left = diameterFast(root->left);
-    pair<int, int> right = diameterFast(root-> right);
-
-
-    int op1 = left.first;
-    int op2 = right.first;
-    int op3 =left.second + right.second;
-
-    pair<int, int> ans;
-    ans.first = max(op1, max(op2, op3));
-    ans.second = max(left.second, right.second) + 1;
-    return ans;
-}
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        return diameterFast(root).first;
+        int res = 0;
+        dfs(root, res);
+        return res;
+    }
+
+private:
+    int dfs(TreeNode* root, int& res) {
+        if (!root) return 0;
+        int left = dfs(root->left, res);
+        int right = dfs(root->right, res);
+        res = max(res, left + right);
+        return 1 + max(left, right);
     }
 };
