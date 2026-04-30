61b093c5d1d935f1d4c3731ca4334c25eb97a8e8
Jeel Patel <pateljeel3105@gmail.com>
2026-04-30 07:46:08 -0400

    Time: 854 ms (14.51%) | Memory: 11.6 MB (29.42%) - LeetSync

--- Solutions/79-word-search/word-search.cpp
+++ Solutions/79-word-search/word-search.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string& word, int r, int c, int index) {
-        if(index >= word.size()) return true;
-        int rows = board.size();
-            int cols = board[0].size();
-            if(r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] != word[index]) return false;
-            char t = board[r][c];
-            board[r][c] = '#';
-            bool found = dfs(board, word, r+1, c, index+1) ||
-                        dfs(board, word, r-1, c, index+1) ||
-                        dfs(board, word, r, c+1, index+1) ||
-                        dfs(board, word, r, c-1, index+1);
-                        
-            board[r][c] = t;
-            return found;
-    }
     bool exist(vector<vector<char>>& board, string word) {
-    int rows = board.size();
-    int cols = board[0].size();
-    for(int r = 0; r < rows; r++) {
-        for(int c = 0; c < cols; c++) {
-            if(dfs(board, word, r, c, 0)) return true;
-        }
-    }
-    return false;
-}
-};
-
-bool dfs(vector<vector<char>>& board, string& word, int r, int c, int index) {
-    if(index >= word.size()) return true;
-    int rows = board.size();
+        int rows = board.size();
         int cols = board[0].size();
-        if(r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] != word[index]) return false;
-        char t = board[r][c];
-        board[r][c] = '#';
-        bool found = dfs(board, word, r+1, c, index+1) ||
-                    dfs(board, word, r-1, c, index+1) ||
-                    dfs(board, word, r, c+1, index+1) ||
-                    dfs(board, word, r, c-1, index+1);
-                    
-        board[r][c] = t;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(dfs(board, word, 0, i, j)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string word, int index, int x, int y){
+        if(index == word.size()) return true;
+        if(x < 0 || y < 0 || x >= board.size() || y >= board[0].size() || board[x][y] != word[index]) return false;
+        char temp = board[x][y];
+        board[x][y] = '#';
+        bool found = dfs(board, word, index + 1, x+1, y) ||
+                     dfs(board, word, index + 1, x-1, y) ||
+                     dfs(board, word, index + 1, x, y+1) ||
+                     dfs(board, word, index + 1, x, y-1);
+        board[x][y] = temp;
         return found;
-}
+    }
+};
