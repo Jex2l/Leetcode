74732f7c9ccd99cad402a78df8748919f17316ba
Jeel Patel <pateljeel3105@gmail.com>
2026-05-07 15:49:48 -0400

    Time: 0 ms (100.00%) | Memory: 28.9 MB (93.38%) - LeetSync

--- Solutions/572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ Solutions/572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -9,20 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-bool sametree(TreeNode* root, TreeNode* subRoot){
-    if(root == nullptr && subRoot == nullptr) return true;
-    if(root == nullptr || subRoot == nullptr) return false;
-    if(root->val != subRoot->val) return false;
-    return sametree(root->left, subRoot->left) && sametree(root->right, subRoot->right);
-}
-
 class Solution {
 public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if(q == nullptr || p== nullptr) return false;
+        if(p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);    
+    }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if(root == nullptr) return false;
-        if(sametree(root, subRoot)) return true;
+        if(isSameTree(root, subRoot)) return true;
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
-
