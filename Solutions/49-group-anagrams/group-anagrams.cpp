bda282809835d7a539b41be9dd7601e0b962b42e
Jeel Patel <pateljeel3105@gmail.com>
2026-08-07 23:14:14 -0400

    Time: 67 ms (11.26%) | Memory: 37.2 MB (5.07%) - LeetSync

--- Solutions/49-group-anagrams/group-anagrams.cpp
+++ Solutions/49-group-anagrams/group-anagrams.cpp
@@ -1,14 +1,44 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         unordered_map<string, vector<string>> x;
+//         vector<vector<string>> ans;
+//         if(strs.size() <= 1) return {strs};
+//         for(int i = 0; i<strs.size(); i++){
+//             string tempq = sort(strs[i].begin(), strs[i].end());
+//             x[tempq].push_back(strs[i]);
+//         }
+//         int y = 0;
+//         for(auto i : x){
+//             for(auto j : i.second){
+//                 ans[y].push_back(j);
+//             }
+//             y++;
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> x;
-        vector<string> d = strs;
-        for(int i =0; i<strs.size();i++){
-            sort(d[i].begin(), d[i].end());
-            x[d[i]].push_back(strs[i]);
-        }
         vector<vector<string>> ans;
-        for(auto i : x){
+        for(string i : strs){
+            vector<int> freq(26, 0);
+            for(char c : i){
+                freq[c - 'a']++;
+            }
+            string tempo = "";
+            for(int i : freq){
+                string e = to_string(i);
+                tempo+=e;
+                tempo+='#';
+            }
+            x[tempo].push_back(i);
+        }
+        for (auto i : x) {
             ans.push_back(i.second);
         }
         return ans;
